0eea9bb41f87eb2aac4979e1fc8efa0a451e6e5f
pchoudhary2210 <pchoudhary@unomaha.edu>
2015-07-31 10:53:26 +0530

    Project code

---
+++ src/main/java/org/uno/IncidenceMgmtSystem/service/UserService.java
@@ -0,0 +1,9 @@
+package org.uno.IncidenceMgmtSystem.service;
+
+import org.uno.IncidenceMgmtSystem.model.*;
+
+public interface UserService {
+	User save(User user);
+	boolean findByLogin(String userName, String password);
+	boolean findByUserName(String userName);
+}
